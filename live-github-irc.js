a2b6531067e46db91dde1cf1a68ac7dba3b7050b
Rob Scanlon <robscanlon@gmail.com>
2013-08-19 21:13:27 -0400

    upgrade streaming to irc interface

--- live-github-irc.js
+++ live-github-irc.js
@@ -4,18 +4,75 @@ var http = require('https'),
     irc = require('irc');
 
 var server = "localhost",
-    nick = "github-bot",
+    nick = "github2-bot",
     channel = "#github";
 
 var last = [];
 
 
-var ircclient = new irc.Client(server, nick, {channels: ["#github"]});
+var ircclient = new irc.Client(server, nick, {debug: false, showErrors: true, floodProtection: false, floodProtectionDelay: 0, channels: ["#github"]});
 
 
-ircclient.addListener('error', function(message) {
-        console.log('irc error: ', message);
+// ircclient.addListener('error', function(message) {
+//         console.log('irc error: ' +  message);
+// });
+
+ircclient.addListener('message', function (from, to, message) {
+        console.log(from + ' => ' + to + ': ' + message);
 });
+
+
+var w = function(c,t){
+    return irc.colors.wrap(c,t);
+}
+
+var shortentype = function(t){
+    if(t == "PullRequestEvent"){
+        return "PR";
+    } else {
+        return t.substring(0,1);
+
+    }
+
+
+}
+
+var createMessage = function(ev){
+    var ret = "";
+
+    if(ev.repository){
+        ret += w("gray","[[") + w('orange',ev.repository.owner + "/" + ev.repository.name) + w("gray","]] ");
+    } else if(ev.payload && ev.payload.target){
+        ret += w("gray","//") + w('orange',ev.payload.target.login) + w("gray","// ");
+    }
+
+    ret += w("light_red", shortentype(ev.type));
+    ret += w("light_magenta", " * ");
+
+    ret += w("dark_blue",ev.url);
+
+    if(ev.actor_attributes && ev.actor_attributes.login){
+        ret += w("light_magenta", " * ") + w("dark_green", ev.actor_attributes.login);
+
+    }
+    
+    if(ev.actor_attributes && ev.actor_attributes.location){
+        ret += w("light_magenta", " * ") + w("yellow", ev.actor_attributes.location);
+
+    }
+    if(ev.repository && ev.repository.language){
+        ret += w("light_magenta", " * ") + w("light_cyan", ev.repository.language);
+
+    }
+    
+
+
+    return ret;
+    
+
+
+
+}
 
 var run = function(){
 
@@ -37,10 +94,19 @@ var run = function(){
             var count = 0;
 
             _.each(newevents, function(val){
+                // if(val.type.substring(0,1)== "F"){
+                //    console.log(val);
+                //    process.exit(0);
+                // }
+
+                // console.log(val);
+                  
                 if(_.contains(last,val.url)){
                     count++;
-                    console.log(val.url);
-                    ircclient.say(channel, JSON.stringify({"url": val.url}));
+                    setTimeout(function(){
+                        ircclient.say(channel,  createMessage(val));
+                        console.log(val.url);
+                    }, Math.random()*6000);
                 }
             }); 
 
